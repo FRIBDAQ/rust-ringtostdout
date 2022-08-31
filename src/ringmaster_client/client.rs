72426e56b55b3b5d78e5a0a9a00ab89544224370
rfoxmich <fox@nscl.msu.edu>
2022-08-31 09:42:29 -0400

    Add use for tcp to talk with ringmaster

--- src/ringmaster_client/client.rs
+++ src/ringmaster_client/client.rs
@@ -1,1 +1,17 @@
 
+//!
+//! The ringmaster_client::client module contains the
+//! code needed to interface with the ringmaster as a client
+//! in this module, we confine ourselves to the REGISTER
+//! operation as we need that to register a client
+//! of the ringbuffer.
+//! We'll provide code to create a client (producer or consumer)
+//! that's been registered with the ringmaster and ready to go.
+//! Note that it is the caller's responsibility to maintain
+//! a connection to the ring master as long as it needs to be a
+//! client. 
+//! 
+//! 
+
+
+use std::net::TcpStream;             // In order to talk with ring master.

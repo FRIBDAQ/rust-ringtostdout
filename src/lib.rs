d90472e2172cee473aba18e6a57cfb4911732130
rfoxmich <fox@nscl.msu.edu>
2022-09-07 07:09:49 -0400

    Reorg docs so that they appear on front page of web docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,25 @@
+//! # ringtostdout program
+//!
+//!  This program attaches as a consumer to a ringbuffer and
+//! takes all the data that buffer has to offer and shoots it out
+//! stdout.  
+//!
+//! ## Usage:
+//!  
+//!     ringtostdout option....
+//! Where the options are:
+//!
+//! *   --directory - the directory with the ringbuffers.  If not
+//! present on the command line defaults to /dev/shm (suitable for
+//! linux)
+//! *   --ring - name of the ring buffer file in that directory
+//! we'll prepend the directory path.
+//! *   --port - Portmanager (not ringmaster) listen port, if not present,
+//! defaults to 30000 the standard.
+//! *   --comment - If present, the supplied text is used to construct
+//! a header visible in system displays of processes.  The ringmaster uses this
+//! to indicate where the ringtostdout programs it spawns off will be sending
+//! its data (ringmaster will have arranged for the stdout of ringtostdout to
+//! be a socket to a client (which will get spawned off to be an stdintoring)).
+
 pub mod ringmaster_client;
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,3 @@
-//! # ringtostdout program
-//!
-//!  This program attaches as a consumer to a ringbuffer and
-//! takes all the data that buffer has to offer and shoots it out
-//! stdout.  
-//!
-//! ## Usage:
-//!  
-//!     ringtostdout option....
-//! Where the options are:
-//!
-//! *   --directory - the directory with the ringbuffers.  If not
-//! present on the command line defaults to /dev/shm (suitable for
-//! linux)
-//! *   --ring - name of the ring buffer file in that directory
-//! we'll prepend the directory path.
-//! *   --port - Portmanager (not ringmaster) listen port, if not present,
-//! defaults to 30000 the standard.
-//! *   --comment - If present, the supplied text is used to construct
-//! a header visible in system displays of processes.  The ringmaster uses this
-//! to indicate where the ringtostdout programs it spawns off will be sending
-//! its data (ringmaster will have arranged for the stdout of ringtostdout to
-//! be a socket to a client (which will get spawned off to be an stdintoring)).
-
 pub mod ringmaster_client;
 use clap::{App, Arg};
 use std::fs;
@@ -94,7 +70,7 @@ fn main() {
 // latency.
 //
 fn output_data(ring: &mut nscldaq_ringbuffer::ringbuffer::consumer::Consumer) {
-    // We must use a vec -- or a static buffer else the buffer will 
+    // We must use a vec -- or a static buffer else the buffer will
     // overflow the stack.  Vec will allocate on the heap,
     // Note that evidently, the vector & can be treated as &[u8] which is
     // what both timed_get and write_all need.
